2ef5fdb3661ac648b874d27db4aecd5100ca60bd
Shoku Takahashi <s21c1072dm@s.chibakoudai.jp>
2022-11-25 18:51:34 +0900

    Add a command

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 ![test](https://github.com/shokutakahashi/robosys2022/actions/workflows/test.yml/badge.svg)
 
 標準入力から読み込んだ数字を足す。
+標準入力から読み込んだ数値を乗ずる
+標準入力から読み込んだ数字の平均を求める。
 
 
 ## 必要なソフトウェア
--- test.bash
+++ test.bash
@@ -1,26 +1,29 @@
 #!/bin/bash -xv
-# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-FileCopyrightText: 2022 Shoku Takahashi
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-            echo NG at Line $1
-            res=1
+	echo NG at Line $1 
+	res=1
 }
 
 res=0
 
 ### I/O TEST ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+out=$(seq  7| ./plus_stdin)
+[ "${out}" = "28 5040 4.0" ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
 
 ### STRANGE INPUT ###
-out=$(echo あ | ./plus)
+out=$(echo あ | ./plus_stdin)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
-out=$(echo | ./plus) #空文字
+out=$(echo | ./plus_stdin) #空文字
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
-  exit $res
+exit $res
